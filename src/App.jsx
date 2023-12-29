aae4fde9fd04afac4de9399590be81ad8f342fca
Kalpesh Dhoke <38274099+kalpesh-d@users.noreply.github.com>
2023-12-29 20:34:20 +0530

    implement responsive grid for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Grid
+        templateAreas={{
+          base: `"header" "main" "footer"`,
+          lg: `"header header" "nav main" "nav footer"`,
+        }}
+      >
+        <GridItem area={"header"} bg="red.300">
+          <Header />
+          header
+        </GridItem>
+        <Show w="10%" above="lg">
+          <GridItem bg="pink.300" area={"nav"}>
+            Nav
+          </GridItem>
+        </Show>
+        <GridItem bg="green.300" area={"main"}>
+          Main
+        </GridItem>
+        <GridItem bg="blue.300" area={"footer"}>
+          Footer
+        </GridItem>
+      </Grid>
     </>
   );
 }
