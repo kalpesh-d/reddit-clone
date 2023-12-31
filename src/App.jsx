da7b716cb2e209f097c31dd402a216353b7e51ee
Kalpesh Dhoke <38274099+kalpesh-d@users.noreply.github.com>
2023-12-31 19:14:13 +0530

    remove nav

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,13 @@ function App() {
       <Grid
         templateAreas={{
           base: `"header" "main" "footer"`,
-          lg: `"header header" "nav main" "nav footer"`,
+          lg: `"header" "main" "footer"`,
         }}
       >
         <GridItem area={"header"}>
           <Navbar />
         </GridItem>
-        <Show w="10%" above="lg">
-          <GridItem bg="pink.300" area={"nav"}>
-            Nav
-          </GridItem>
-        </Show>
-        <GridItem bg="green.300" area={"main"}>
+        <GridItem area={"main"}>
           <PostGrid />
         </GridItem>
         <GridItem bg="blue.300" area={"footer"}>
